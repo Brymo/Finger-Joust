5399e3bc6fea205e0d9a88e3e5531ea81b915748
Brymo <bryanmoh@yahoo.com.au>
2019-10-05 01:35:35 +1000

    Added sound, no duplicates

--- js/scripts.js
+++ js/scripts.js
@@ -14,37 +14,46 @@ document.onkeypress = function (e) {
     const goalbarA = document.getElementById("ga");
     const goalbarB = document.getElementById("gb");
 
-    console.log(key+" "+targetA);
-
     if(key === targetA){
         scoreA++;
         targetA = randomChar();
         playerA.innerHTML = targetA;
-        targetB = randomChar();
+        targetB = randomCharButNot(targetA);
         playerB.innerHTML = targetB;
         goalbarA.style.width = scoreA * 10 + "%";
+        var audio = new Audio('./bellA.wav');
+        audio.play();
     }
 
     if(key === targetB){
         scoreB++;
         targetA = randomChar();
         playerA.innerHTML = targetA;
-        targetB = randomChar();
+        targetB = randomCharButNot(targetA);
         playerB.innerHTML = targetB;
         goalbarB.style.width = scoreB * 10 + "%";
+        var audio = new Audio('./bellB.wav');
+        audio.play();
     }
 
     if(scoreA == 10){
         setTimeout(() => {
             playerA.innerHTML = "Winner";
         }, 0);
+
+        document.onkeypress = null;
+
     }
 
     if(scoreB == 10){
         setTimeout(() => {
             playerB.innerHTML = "Winner";
         }, 0);
+
+        document.onkeypress = null;
     }
+
+    
     
 };
 
@@ -55,6 +64,23 @@ function randomChar(){
     }
 
     return String.fromCharCode(97 + getRandomInt(25));
+
+}
+
+function randomCharButNot(thisOne){
+
+    const getRandomInt = (max)=>{
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    
+    let char = String.fromCharCode(97 + getRandomInt(25));
+
+    while(char === thisOne){
+        char = String.fromCharCode(97 + getRandomInt(25));
+    }
+
+    return char;
 
 }
 
