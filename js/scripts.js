ca0f867085e9786384839f4d9cd2fd8f5dffdfd3
Brymo <bryanmoh@yahoo.com.au>
2019-10-07 20:57:08 +1100

    Everything we need

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 const playerA = document.getElementById("P1");
-const playerL = document.getElementById("P2");
+const playerB = document.getElementById("P2");
 const goalbarA = document.getElementById("ga");
-const goalbarL = document.getElementById("gl");
-const displays = {a: playerA, l:playerL};
-const listenerA = document.addEventListener('keyup', listener(goalbarA,"a","./bellA.wav"));
-const listenerL = document.addEventListener('keyup', listener(goalbarL,"l","./bellL.wav"));
+const goalbarB = document.getElementById("gb");
+const displays = {a: playerA, b:playerB};
+const startButton = document.getElementById("init");
+
+startButton.addEventListener("click",()=>{
+
+    let count = 3;
+    startButton.innerHTML = 3;
+
+    const countdown = setInterval(()=>{
+
+        count--;
+
+        if(count === 0){
+            startButton.style.visibility = "hidden";
+            clearInterval(countdown);
+            document.addEventListener('keyup', listener(goalbarA,"a","./bellA.wav"));
+            document.addEventListener('keyup', listener(goalbarB,"b","./bellB.wav"));
+        }else{
+            startButton.innerHTML = count;
+        }
+    },1000);
+
+});
+
 
 let finished = false;
 function listener(goal,displayKey,noise){
     let score = 0;
-    displays[displayKey].innerHTML = displayKey;
+    displays[displayKey].innerHTML = randomCharButNot()
 
     return ()=>{
         const key = event.key || event.keyCode;
@@ -27,7 +48,7 @@ function listener(goal,displayKey,noise){
             
             const usedChars = [];
             Object.keys(displays).forEach((key)=>{
-                const randomChar = randomCharButNot(usedChars);
+                const randomChar = randomCharButNot(...usedChars);
                 usedChars.push(randomChar);
                 setTimeout(()=>{displays[key].innerHTML = randomChar},0);
             });
@@ -35,7 +56,7 @@ function listener(goal,displayKey,noise){
     }
 }
 
-function randomCharButNot(theseOnes){
+function randomCharButNot(...theseOnes){
     const getRandomInt = (max)=>{
         return Math.floor(Math.random() * Math.floor(max));
     }
