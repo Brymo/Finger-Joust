22d372b54e4082f827c818ca4a9aee28ef2684bf
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-21 12:55:52 +1100

    Format code

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,63 @@
-const playerA = document.getElementById("P1");
-const playerB = document.getElementById("P2");
-const goalbarA = document.getElementById("ga");
-const goalbarB = document.getElementById("gb");
-const startButton = document.getElementById("init");
+const playerA = document.getElementById('P1')
+const playerB = document.getElementById('P2')
+const goalbarA = document.getElementById('ga')
+const goalbarB = document.getElementById('gb')
+const startButton = document.getElementById('init')
 
-startButton.addEventListener("click",()=>{
-    [playerA, playerB].forEach(player => player.usedChars = []);
+startButton.addEventListener('click', () => {
+  ;[playerA, playerB].forEach(player => (player.usedChars = []))
 
-    startButton.innerHTML = 3;
+  startButton.innerHTML = 3
 
-    let count = 3;
-    const countdown = setInterval(()=>{
-        count--;
-        if(count === 0){
-            startButton.style.visibility = "hidden";
-            clearInterval(countdown);
-            document.addEventListener('keyup', listener(goalbarA,playerA,"./bellA.wav"));
-            document.addEventListener('keyup', listener(goalbarB,playerB,"./bellB.wav"));
-        }else{
-            startButton.innerHTML = count;
-        }
-    },1000);
-
-});
-
-let finished = false;
-function listener(goal,elem,noise){
-    let score = 0;
-    elem.innerHTML = randomCharButNot()
-
-    return (evt)=>{
-        const key = event.key.toLowerCase() || event.keyCode;
-        
-        if(key === elem.innerHTML && !finished){
-            new Audio(noise).play();
-
-            score++;
-            goal.style.width = score * 10 + "%";
-            if(score == 10){
-                elem.innerHTML = "Winner";
-                finished = true;
-                return;
-            }
-            
-            [playerA, playerB].forEach(player => {
-                let randomChar = randomCharButNot(...player.usedChars);
-                player.usedChars.push(randomChar);
-                player.innerHTML = randomChar;
-            });
-        }
+  let count = 3
+  const countdown = setInterval(() => {
+    count--
+    if (count === 0) {
+      startButton.style.visibility = 'hidden'
+      clearInterval(countdown)
+      document.addEventListener(
+        'keyup',
+        listener(goalbarA, playerA, './bellA.wav')
+      )
+      document.addEventListener(
+        'keyup',
+        listener(goalbarB, playerB, './bellB.wav')
+      )
+    } else {
+      startButton.innerHTML = count
     }
+  }, 1000)
+})
+
+let finished = false
+function listener (goal, elem, noise) {
+  let score = 0
+  elem.innerHTML = randomCharButNot()
+
+  return evt => {
+    const key = event.key.toLowerCase() || event.keyCode
+
+    if (key === elem.innerHTML && !finished) {
+      new Audio(noise).play()
+
+      score++
+      goal.style.width = score * 10 + '%'
+      if (score == 10) {
+        elem.innerHTML = 'Winner'
+        finished = true
+        return
+      }
+
+      ;[playerA, playerB].forEach(player => {
+        let randomChar = randomCharButNot(...player.usedChars)
+        player.usedChars.push(randomChar)
+        player.innerHTML = randomChar
+      })
+    }
+  }
 }
 
-function randomCharButNot(...theseOnes){
-    let char = String.fromCharCode(97 + Math.floor(Math.random() * 26));
-    return theseOnes.includes(char) ? randomCharButNot(...theseOnes) : char;
+function randomCharButNot (...theseOnes) {
+  let char = String.fromCharCode(97 + Math.floor(Math.random() * 26))
+  return theseOnes.includes(char) ? randomCharButNot(...theseOnes) : char
 }
-
-
-
